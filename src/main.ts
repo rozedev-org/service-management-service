bdb4727c80476379d59e80b35fdd05bac71a8c5a
Juan C Jimenez <juancarlo14071@gmail.com>
2024-02-27 18:57:50 -0400

    Add CORS support to the application

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   fs.writeFileSync('./docs/swagger.json', JSON.stringify(document));
-
+  app.enableCors();
   await app.listen(process.env.PORT || 8000, '0.0.0.0');
   Logger.log(`Application is running on: ${await app.getUrl()}`, 'Main');
 }
