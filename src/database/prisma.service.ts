d55a948d19746ef9f34c08599ec6ca6627c952d4
Juan C Jimenez <juancarlo14071@gmail.com>
2024-04-01 21:13:28 -0400

    Add default requirement state if not found

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -5,5 +5,19 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    const defaultState = await this.requirementState.findUnique({
+      where: {
+        id: 1
+      }
+    });
+
+    if (!defaultState) {
+      await this.requirementState.create({
+        data: {
+          title: 'To do',
+          secuence: 1
+        }
+      });
+    }
   }
 }
