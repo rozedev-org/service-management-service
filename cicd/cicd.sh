b392d4d0e7a0cedee03314a0bb59df713f09ade9
Juan C Jimenez <juancarlo14071@gmail.com>
2024-05-11 20:19:31 -0400

    chore: Update Kubernetes commands in cicd.sh to use correct directory for Kubernetes deployment

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -22,7 +22,7 @@ echo "Empuje de la imagen de Docker completado con éxito."
 
 # Desplegar en Kubernetes
 echo "Iniciando despliegue en Kubernetes..."
-microk8s kubectl apply -k ./ || handle_error "despliegue 1"
+microk8s kubectl apply -k ../k8s || handle_error "despliegue 1"
 microk8s kubectl set image deployment/$appName $appName=$image -n service-management || handle_error "despliegue 2"
 microk8s kubectl rollout restart deployment/$appName -n service-management || handle_error "despliegue 3"
 echo "Despliegue en Kubernetes completado con éxito."
