2b7a7e5052047906bcdce386075b9f754ef447e0
Juan C Jimenez <juancarlo14071@gmail.com>
2024-05-21 17:01:41 -0400

    chore: Refactor PrismaService initialization and remove commented code

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -5,30 +5,30 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-    const defaultState = await this.requirementState.findMany({});
+    // const defaultState = await this.requirementState.findMany({});
 
-    if (!defaultState.length) {
-      await this.requirementState.create({
-        data: {
-          title: 'To do',
-          secuence: 1
-        }
-      });
-    }
+    // if (!defaultState.length) {
+    //   await this.requirementState.create({
+    //     data: {
+    //       title: 'To do',
+    //       secuence: 1
+    //     }
+    //   });
+    // }
 
-    const defaultUser = await this.user.findUnique({
-      where: { userName: 'admin' }
-    });
-    if (!defaultUser) {
-      const adminUser = await this.user.create({
-        data: {
-          userName: 'admin',
-          password: process.env.DEFAULT_ADMIN_PASS || 'admin',
-          lastName: 'Admin',
-          firstName: 'Admin'
-        }
-      });
-      console.log(adminUser);
-    }
+    // const defaultUser = await this.user.findUnique({
+    //   where: { userName: 'admin' }
+    // });
+    // if (!defaultUser) {
+    //   const adminUser = await this.user.create({
+    //     data: {
+    //       userName: 'admin',
+    //       password: process.env.DEFAULT_ADMIN_PASS || 'admin',
+    //       lastName: 'Admin',
+    //       firstName: 'Admin'
+    //     }
+    //   });
+    //   console.log(adminUser);
+    // }
   }
 }
